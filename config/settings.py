bbd2a69cc89da2eb6dae454bfdab5806845cfe60
Rozievich <blackcoder459@outlook.com>
2023-07-30 15:18:56 +0500

    Dockerga ulandi

--- config/settings.py
+++ config/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Portfolio_db',
+        'PASSWORD': 'black090',
+        'USER': 'postgres',
+        'HOST': 'pg',
+        'PORT': '5432',
     }
 }
 
